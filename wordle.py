bf6f81d64d46586c60f3f9c6892bd8cc764ea9e3
Jesse Bordoe <jesse.bordoe@gmail.com>
2022-01-23 13:13:53 +0100

    add naive player

--- wordle.py
+++ wordle.py
@@ -13,7 +13,7 @@ class HumanPlayer(PlayerInterface):
     def guess(self, game_state) -> str:
         while True:
             print("""
-        Enter your guess! Must be a {}-letter word
+Enter your guess! Must be a {}-letter word
     """.format(game_state.word_length))
             guess = input().strip().upper()
             if not guess in game_state.wordlist:
@@ -21,17 +21,55 @@ class HumanPlayer(PlayerInterface):
             else:
                 return guess
 
+class NaivePlayer(PlayerInterface):
+    def __init__(self, game_state):
+        self.known = ['' for _ in range(game_state.word_length)]
+        self.found = set()
+        self.guessed = set()
+
+    def guess(self, game_state) -> str:
+        found = self.found.union(game_state.found)
+        known = [e[0] or e[1] for e in zip(self.known, game_state.known)]
+
+        known_str = ''.join(known)
+        if len(known_str) == game_state.word_length:
+            return known_str
+
+        candidates = []
+        # TODO: all this linear scanning is inefficient
+        # Can we use/build a tree structure perhaps?
+        for w in game_state.wordlist:
+            invalid = False
+            if w in self.guessed: continue
+            for i in range(game_state.word_length):
+                if known[i] and (known[i] != w[i]):
+                    invalid = True
+                    break
+            if invalid or not set(w).issuperset(found): continue
+
+            candidates.append(w)
+
+        if not candidates:
+            print("WTF, no candidates available!")
+        else:
+            guess = random.sample(candidates, 1)[0].upper()
+            self.guessed.add(guess)
+            self.known = known
+            self.found = found
+            return guess
+
+
 class GameState:
     def __init__(self, wordlist, wordlen=5):
         self.wordlist = wordlist
         self.word_length = wordlen
-        self.known = ["_" for _ in range(wordlen)]
+        self.known = ['' for _ in range(wordlen)]
         self.found = set()
         self.answer = random.sample(wordlist, 1)[0].upper()
         self.guesses = 0
 
     def update(self, guess):
-        known_new = [None for _ in range(self.word_length)]
+        known_new = ['' for _ in range(self.word_length)]
 
         for i in range(len(guess)):
             gc = guess[i]
@@ -68,17 +106,17 @@ def game(wordlen=5):
     all_words = requests.get(WORDLIST_URL).json().keys()
     words = [w.upper() for w in all_words if len(w) == wordlen]
 
-    player = HumanPlayer()
     state = GameState(words, wordlen=wordlen)
-
-    print(' '.join([c for c in state.known]))
+#    player = HumanPlayer()
+    player = NaivePlayer(state)
+    print(' '.join([c or "_" for c in state.known]))
 
     while True:
-        print("_________ GUESS #{} _________".format(state.guesses + 1))
+        print("========= GUESS #{} =========".format(state.guesses + 1))
         guess = player.guess(state)
+        state.update(guess)
         for i in range(len(guess)):
             gc = guess[i]
-            state.update(guess)
             if gc == state.answer[i]:
                 cprint(gc, 'white', 'on_green', end='')
             elif gc in state.answer:
