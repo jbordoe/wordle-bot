7fcb55e1a4a9d5d36bb59a5cfef149b1d12c9ce1
Jesse Bordoe <jesse.bordoe@gmail.com>
2025-07-11 22:11:23 +0200

    enable wildcard player imports

---
+++ lib/player/__init__.py
@@ -0,0 +1,15 @@
+from lib.player.bot_player import BotPlayer
+from lib.player.human_player import HumanPlayer
+from lib.player.llm_player import LLMPlayer
+from lib.player.nn_player import NNPlayer
+from lib.player.player_interface import PlayerInterface
+from lib.player.player_knowledge import PlayerKnowledge
+
+__all__ = [
+    "BotPlayer",
+    "HumanPlayer",
+    "LLMPlayer",
+    "NNPlayer",
+    "PlayerInterface",
+    "PlayerKnowledge",
+]
