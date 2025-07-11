0f71d0d4f64d17603ba13ebfb9f25054ce707db4
Jesse Bordoe <jesse.bordoe@gmail.com>
2025-07-11 22:10:07 +0200

    improve evaluation script

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -1,33 +1,60 @@
 import argparse
+import logging
+import multiprocessing
 
-from alive_progress import alive_bar
 from termgraph.module import Args, BarChart, Colors, Data
+from tqdm import tqdm
 
 from lib.game.dummy_wordle_game import DummyWordleGame
-from lib.player.bot_player import BotPlayer
+from lib.player.nn_player import NNPlayer
 from lib.word_scorer.statistical_word_scorer import StatisticalWordScorer
 from lib.words.word_index import WordIndex
 from lib.words.word_loader import WordLoader
 
+# Global variables for each worker process
+_word_index = None
+_word_scorer = None
+_word_list = WordLoader.load_wordlist()
 
-def go(runs, sample_size):
-    wordlen = 5
-    word_list = WordLoader.load_wordlist(sample_size=sample_size)
+def _initializer():
+    """
+    Initializes WordIndex and StatisticalWordScorer once per worker process.
+    """
+    global _word_index, _word_scorer
+    _word_index = WordIndex(_word_list)
+    _word_scorer = StatisticalWordScorer(_word_list, b=0.2)
 
+def go(runs, sample_size, num_procs):
     print("running games")
     guesses = {}
-    words = WordIndex(word_list)
-    ranker = StatisticalWordScorer(word_list)
-    with alive_bar(runs, bar="filling", spinner="dots") as bar:
-        for i in range(runs):
-            n_guesses = game(words, ranker, wordlen=wordlen)
-            if n_guesses in guesses:
-                guesses[n_guesses] += 1
-            else:
-                guesses[n_guesses] = 1
-            bar()
+
+    logging.info(f"Using {num_procs} processes for game simulation.")
+
+    with multiprocessing.Pool(processes=num_procs, initializer=_initializer) as pool:
+        with tqdm(total=runs, desc="Simulating games") as bar:
+            for round_num in pool.imap_unordered(game_worker, range(runs)):
+                guesses[round_num] = guesses.get(round_num, 0) + 1
+                bar.update(1)
     output_results(guesses)
 
+def game_worker(_):
+    # Each worker process uses the globally initialized components
+    game_state = DummyWordleGame(_word_index.list)
+    # player = BotPlayer(state, words=_word_index, word_scorer=_word_scorer)
+    player = NNPlayer(game_state, words=_word_index)
+    result = None
+    round_num = 0
+    maxrounds = 10
+
+    while True:
+        round_num += 1
+        guess = player.guess(game_state, prev=result)
+        result = game_state.update(guess)
+        player.update_state(result)
+
+        if result.correct or round_num >= maxrounds:
+            break
+    return round_num
 
 def output_results(guesses):
     print(f"""
@@ -52,21 +79,7 @@ RESULTS
     )
     chart.draw()
 
-
-def game(words, ranker, wordlen=5):
-    state = DummyWordleGame(words.list)
-    player = BotPlayer(state, words=words, ranker=ranker)
-    result = None
-    while True:
-        guess = player.guess(state, prev=result)
-        result = state.update(guess)
-
-        if guess == state.answer:
-            return state.guesses
-        else:
-            player.update_state(result)
-
-
+cores = multiprocessing.cpu_count()
 parser = argparse.ArgumentParser(
     description="Run several wordle games and output the results"
 )
@@ -80,10 +93,22 @@ parser.add_argument(
     required=False,
     help="Number of words to sample (default: all)",
 )
+parser.add_argument(
+    "-t",
+    "--threads",
+    type=int,
+    default=cores,
+    help=f"Number of threads to use (default: {cores})",
+)
 
 args = parser.parse_args()
 
+threads = args.threads
 runs = args.runs
 sample_size = args.sample_size
 
-go(runs, sample_size)
+if threads > multiprocessing.cpu_count():
+    logging.warning(f"Requested {threads} threads, but only {cores} available.")
+    threads = multiprocessing.cpu_count()
+
+go(runs, sample_size, threads)
