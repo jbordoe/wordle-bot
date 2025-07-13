580f0b1d027c1ea1074bc091ea94373a37fd92de
Jesse Bordoe <jesse.bordoe@gmail.com>
2025-07-13 17:26:45 +0200

    eneable player selection in eval script

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -6,6 +6,7 @@ from termgraph.module import Args, BarChart, Colors, Data
 from tqdm import tqdm
 
 from lib.game.dummy_wordle_game import DummyWordleGame
+from lib.player.bot_player import BotPlayer
 from lib.player.nn_player import NNPlayer
 from lib.word_scorer.statistical_word_scorer import StatisticalWordScorer
 from lib.words.word_index import WordIndex
@@ -16,21 +17,26 @@ _word_index = None
 _word_scorer = None
 _word_list = WordLoader.load_wordlist()
 
-def _initializer():
+def _initializer(player_type):
     """
     Initializes WordIndex and StatisticalWordScorer once per worker process.
     """
-    global _word_index, _word_scorer
+    global _word_index, _word_scorer, _player_type
+    _player_type = player_type
     _word_index = WordIndex(_word_list)
     _word_scorer = StatisticalWordScorer(_word_list, b=0.2)
 
-def go(runs, sample_size, num_procs):
+def go(runs, sample_size, num_procs, player):
     print("running games")
     guesses = {}
 
     logging.info(f"Using {num_procs} processes for game simulation.")
 
-    with multiprocessing.Pool(processes=num_procs, initializer=_initializer) as pool:
+    with multiprocessing.Pool(
+        processes=num_procs,
+        initializer=_initializer,
+        initargs=(player,),
+    ) as pool:
         with tqdm(total=runs, desc="Simulating games") as bar:
             for round_num in pool.imap_unordered(game_worker, range(runs)):
                 guesses[round_num] = guesses.get(round_num, 0) + 1
@@ -40,8 +46,11 @@ def go(runs, sample_size, num_procs):
 def game_worker(_):
     # Each worker process uses the globally initialized components
     game_state = DummyWordleGame(_word_index.list)
-    # player = BotPlayer(state, words=_word_index, word_scorer=_word_scorer)
-    player = NNPlayer(game_state, words=_word_index)
+    if _player_type == "bot":
+        player = BotPlayer(game_state, words=_word_index, word_scorer=_word_scorer)
+    elif _player_type == "nn":
+        player = NNPlayer(game_state, words=_word_index)
+
     result = None
     round_num = 0
     maxrounds = 10
@@ -100,15 +109,27 @@ parser.add_argument(
     default=cores,
     help=f"Number of threads to use (default: {cores})",
 )
+parser.add_argument(
+    "-p",
+    "--player",
+    type=str,
+    default="bot",
+    help="Player to use. Either 'bot' or 'nn' (default: bot)",
+)
 
 args = parser.parse_args()
 
 threads = args.threads
 runs = args.runs
 sample_size = args.sample_size
+player = args.player
 
 if threads > multiprocessing.cpu_count():
     logging.warning(f"Requested {threads} threads, but only {cores} available.")
     threads = multiprocessing.cpu_count()
 
-go(runs, sample_size, threads)
+if player not in ["bot", "nn"]:
+    logging.error(f"Invalid player type: {player}")
+    exit(1)
+
+go(runs, sample_size, threads, player)
